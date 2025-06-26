e0dac8b39c8c288c0290c75131b2a9899a59dfd9
Sreekanth <spayyavula@gmail.com>
2025-06-26 09:36:08 -0700

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+  },
+  esbuild: {
+    target: 'esnext',
+  },
 });
